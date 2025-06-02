9abeb3231fd3d9cdcb03fd4ae82a56164b795c0c
Carina Serra <86061577+MikuSoo@users.noreply.github.com>
2025-06-02 11:17:52 +0100

    update

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 let email_container_element, email_a_element, email_button_element;
 let temp_input;
 
+let hellos_container_element, hellos_element;
+let hellos, temp_hellos_random_index, hellos_random_index;
 
 function setup() 
 {
   noCanvas();
-  
+/*JAVA PARA O Copylink do email*/
   email_container_element = select ("#email_container");
   
   email_a_element = select ("#email");
@@ -14,6 +16,18 @@ function setup()
   email_button_element.parent (email_container_element);
   email_button_element.hide();
   email_button_element.mousePressed (copyEmail);
+
+
+
+  /*JAVA PARA O HELLO*/
+  temp_hellos_random_index = 0;
+  hellos_random_index = 0;
+  
+  hellos = [];
+  hellos[0] = "olá";
+  hellos[1] = "hello";
+  
+  hellos_container_element = select("#hellos_container");
 }
 
 
@@ -52,3 +66,4 @@ function copyEmailFallback()
   
   console.log ("Email copied to clipboard");
 }
+
