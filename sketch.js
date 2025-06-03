5d26ea6139f69378d754994926ed272221200500
Carina Serra <86061577+MikuSoo@users.noreply.github.com>
2025-06-03 18:41:23 +0100

    update JS

--- sketch.js
+++ sketch.js
@@ -4,8 +4,7 @@ let temp_input;
 let hellos_container_element, hellos_element;
 let hellos, temp_hellos_random_index, hellos_random_index;
 
-function setup() 
-{
+function setup(){
   noCanvas();
 /*JAVA PARA O Copylink do email*/
   email_container_element = select ("#email_container");
@@ -19,7 +18,7 @@ function setup()
 
 
 
-  /*JAVA PARA O HELLO*/
+  /*JAVA PARA O HELLO-Random*/
   temp_hellos_random_index = 0;
   hellos_random_index = 0;
   
@@ -35,14 +34,15 @@ function setup()
 }
 
 
-function copyEmail() 
-{
+function copyEmail(){
   temp_input = createInput();
   temp_input.parent (email_container_element);
-  temp_input.value (email_a_element.html());
+  temp_input.value (email_a_element.elt.innerText);
   
+  console.log(temp_input.elt.value);
+
   try {
-    navigator.clipboard.writeText (temp_input.elt.select())
+    navigator.clipboard.writeText (temp_input.elt.value)
       .then(() => {
           temp_input.remove();
           email_button_element.hide();
@@ -60,8 +60,7 @@ function copyEmail()
 }
 
 
-function copyEmailFallback() 
-{
+function copyEmailFallback(){
   temp_input.elt.select();
   document.execCommand ('copy');
   
@@ -71,21 +70,28 @@ function copyEmailFallback()
   console.log ("Email copied to clipboard");
 }
 
-function draw() 
+function showEmailButton() 
 {
-  if (select("#hellos") != null) 
-  {
+  email_button_element.show();
+}
+
+
+function hideEmailButton() 
+{
+  email_button_element.hide();
+}
+
+function draw(){
+  if (select("#hellos") != null){
     hellos_element = select ("#hellos");
     hellos_element.remove();
   }
-  else 
-  {
+  else{
     frameRate (0.5);
   }
   
-  while (temp_hellos_random_index === hellos_random_index) 
-  {
-    temp_hellos_random_index = int( random(0,hellos.length) );
+  while (temp_hellos_random_index === hellos_random_index){
+    temp_hellos_random_index = int(random(0,hellos.length));
   }
   
   hellos_random_index = temp_hellos_random_index;
