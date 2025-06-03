b9a353c1719819ebad5ee5d74e90493651e896ff
Carina Serra <86061577+MikuSoo@users.noreply.github.com>
2025-06-03 19:08:14 +0100

    update

--- hellos.js
+++ hellos.js
@@ -1,6 +1,10 @@
 let email_container_element, email_a_element, email_button_element;
 let temp_input;
 
+
+let hellos_container_element, hellos_element;
+let hellos, temp_hellos_random_index, hellos_random_index;
+
 function setup(){
   noCanvas();
 /*JAVA PARA O Copylink do email*/
@@ -12,6 +16,41 @@ function setup(){
   email_button_element.parent (email_container_element);
   email_button_element.hide();
   email_button_element.mousePressed (copyEmail);
+
+  /*JAVA PARA O HELLO-Random*/
+  temp_hellos_random_index = 0;
+  hellos_random_index = 0;
+  
+  hellos = [];
+  hellos[0] = "olá";
+  hellos[1] = "hello";
+  hellos[2] = "ciao";
+  hellos[3] = "여보!";
+  hellos[4] = "你好";
+  hellos[5] = "こんにちは";
+
+  hellos_container_element = select("#hellos_container");
+}
+
+function draw(){
+  if (select("#hellos") != null){
+    hellos_element = select ("#hellos");
+    hellos_element.remove();
+  }
+  else{
+    frameRate (0.5);
+  }
+  
+  while (temp_hellos_random_index === hellos_random_index){
+    temp_hellos_random_index = int(random(0,hellos.length));
+  }
+  
+  hellos_random_index = temp_hellos_random_index;
+  
+  hellos_element = createP (hellos[hellos_random_index]);
+  hellos_element.id ("hellos");
+  
+  hellos_element.parent (hellos_container_element);
 }
 
 
